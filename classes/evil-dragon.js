49fe68b3b10e0fbbacba802115f0bf2c4dac174f
Logan French <Loganfrench04@gmail.com>
2023-10-05 20:28:58 -0400

    Solved Phase 3

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,36 @@
-// Your code here
+const Dragon = require("./dragon")
+
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings = [], nemesis) {
+        super(name, color)
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner() {
+        for (let evilDoing of this.evilDoings) {
+            console.log(`${this.name} will ${evilDoing}`)
+        }
+    }
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`
+    }
+}
+
+const drogon = new EvilDragon(
+    "Drogon",
+    "black-red",
+    [
+        "eat all your livestock",
+        "burn down your castle in Westeros",
+        "burn your father and brother"
+    ],
+    "Night King"
+);
+console.log(drogon);
+drogon.dontInviteThemOverForDinner();
+console.log(drogon.breathesFire());
+console.log(drogon.burnsNemesis());
+
+module.exports = EvilDragon;
